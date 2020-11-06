4e2ab72732edaaf71f710f3721c0405d68d80cd8
David GABISON <david.gabison@outlook.com>
2020-11-06 09:58:36 +0100

    first version

---
+++ index.js
@@ -0,0 +1,24 @@
+const DEFAULT_DELAY = 200;
+
+function getDelayedCallback(cb, delay) {
+  if (typeof cb === "number") {
+    cb, delay = delay, cb
+  }
+
+  delay = delay || DEFAULT_DELAY
+
+  if (typeof cb === "undefined")
+    return callback => getDelayedCallback(callback, delay);
+
+  if (typeof cb !== "function")
+    throw new TypeError(`callback parameter must be a function, ${typeof cb} given`);
+
+  let timeout = null;
+  return (...args) => {
+    const self = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => cb.call(self, ...args), delay);
+  };
+}
+
+module.exports = getDelayedCallback;
