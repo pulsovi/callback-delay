27a5c002df33455da2cc658218ce422d02340e35
David GABISON <david.gabison@outlook.com>
2020-11-06 13:18:38 +0100

    fix: this in arrow function

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
+/* eslint-disable no-param-reassign, consistent-this, no-invalid-this, prefer-reflect */
 const DEFAULT_DELAY = 200;
 
 function getDelayedCallback(cb, delay) {
-  if (typeof cb === "number") {
-    cb, delay = delay, cb
-  }
+  if (typeof cb === "number") [cb, delay] = [delay, cb];
 
-  delay = delay || DEFAULT_DELAY
+  delay = delay || DEFAULT_DELAY;
 
   if (typeof cb === "undefined")
     return callback => getDelayedCallback(callback, delay);
@@ -14,8 +13,9 @@ function getDelayedCallback(cb, delay) {
     throw new TypeError(`callback parameter must be a function, ${typeof cb} given`);
 
   let timeout = null;
-  return (...args) => {
+  return function delayedCallback(...args) {
     const self = this;
+
     clearTimeout(timeout);
     timeout = setTimeout(() => cb.call(self, ...args), delay);
   };
